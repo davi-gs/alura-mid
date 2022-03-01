e2f356629a0a12324ce9c78d6296adbfe81ef270
David <davidpvh2020@gmail.com>
2022-02-28 22:08:39 -0400

    adicionando aos demais botões e refatorando o código

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++){
+
+    const tecla = listaDeTeclas[indice];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    } 
+    //console.log(contador);
+}
